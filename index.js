e4710fa2213acc3a8b2b2aba758a95edd15c25b8
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-07-24 14:17:24 +0600

    single college data fetched

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5nrgbhc.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -21,13 +21,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // college collection
     const collegeCollection = client.db("CampusDB").collection("colleges");
 
+    //all college data displayed
     app.get("/colleges", async (req, res) => {
       const result = await collegeCollection.find().toArray();
       res.send(result);
     });
 
+    //single college data displayed
+    app.get("/college/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await collegeCollection.findOne(query);
+      res.send(result);
+    });
+
+    // reviews collection
     const reviewsCollection = client.db("CampusDB").collection("reviews");
 
     app.get("/reviews", async (req, res) => {
